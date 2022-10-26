c2fb52ad521a3e1787cb951e3ebdb851b49d0f51
Yeimi Ceballos <202100100@upqroo.edu.mx>
2022-10-26 11:57:26 -0500

    Se estableció acceso a la base de datos, linea 17-35 del homecontroller

--- Proyecto24BM/Controllers/HomeController.cs
+++ Proyecto24BM/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Proyecto24BM.Models;
+using Proyecto24BM.Models.Contest;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,16 +14,20 @@ namespace Proyecto24BM.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDBContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDBContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+       public async Task<IActionResult> Index()
+        {
+            var response = await _context.Usuarios.ToListAsync();
+                return View();
         }
         //retorna las vitas
-        public IActionResult Index()
-        {
-            return View();
-        }
+
 
         public IActionResult Privacy()
         {
